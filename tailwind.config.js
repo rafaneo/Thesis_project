0101b73d16022d4c6ef7f6237eff84b001b6da67
Savvas Christodoulou <ch.savvas28@hotmail.com>
2024-04-14 22:23:36 +0100

    Require tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.{html,js}'],
-	theme: {
-		extend: {
-		  gridTemplateRows: {
-			'[auto,auto,1fr]': 'auto auto 1fr',
-		  },
-		},
-	  },
-	plugins: [require('@tailwindcss/aspect-ratio')],
-};
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+  ],
+}
