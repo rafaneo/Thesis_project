00d87826ab70122e3dc9975d54e57d939d471f02
chsavvas11 <ch.savvas28@hotmail.com>
2024-04-26 20:55:42 +0100

    User settings schema and form

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ import Footer from './components/footer';
 import Web3_Storage from './components/w3Storage';
 import CreateListing from './components/create_listing';
 import './index.css';
+import EditUserSettings from './components/edit-user-settings';
 
 function App() {
   return (
-    <div className="">
+    <div className=''>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path="/" element={<Marketplace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/product" element={<Example />} /> {/*This is for testing */}
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/web3_storage" element={<Web3_Storage />} />
-          <Route path="/create_listing" element={<CreateListing />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path='/' element={<Marketplace />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/product' element={<Example />} />{' '}
+          {/*This is for testing */}
+          <Route path='/product/:id' element={<Product />} />
+          <Route path='/web3_storage' element={<Web3_Storage />} />
+          <Route path='/create_listing' element={<CreateListing />} />
+          <Route path='/settings' element={<EditUserSettings />} />
+          <Route path='*' element={<h1>Not Found</h1>} />
         </Routes>
         <Footer />
       </Router>
