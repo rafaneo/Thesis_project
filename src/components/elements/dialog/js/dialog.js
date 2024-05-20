66aa567f016907d4750a7f2db38490e7eba4a3f0
Savvas Christodoulou <ch.savvas28@hotmail.com>
2024-05-20 12:49:15 +0300

    Add callback options to dialog

--- src/components/elements/dialog/js/dialog.js
+++ src/components/elements/dialog/js/dialog.js
@@ -5,13 +5,23 @@ import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 export default function ModalDialog({
   title,
   text,
-  buttonText,
-  //   onCancel,
-  //   onConfirm,
+  buttonText = 'Submit',
+  onCancel,
+  onConfirm,
 }) {
   const [open, setOpen] = useState(true);
 
   const cancelButtonRef = useRef(null);
+
+  const handleCancel = () => {
+    if (onCancel) onCancel();
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    if (onConfirm) onConfirm();
+    setOpen(false);
+  };
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -69,17 +79,17 @@ export default function ModalDialog({
                   <button
                     type='button'
                     className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
-                    onClick={() => setOpen(false)}
+                    onClick={handleCancel}
+                    ref={cancelButtonRef}
                   >
-                    {buttonText}
+                    Cancel
                   </button>
                   <button
                     type='button'
                     className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
+                    onClick={handleSubmit}
                   >
-                    Cancel
+                    {buttonText}
                   </button>
                 </div>
               </Dialog.Panel>
